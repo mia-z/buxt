2d450a110068b4c544369945b8787368d4480003
mia-z <iperish.x@gmail.com>
2022-11-10 01:17:58 +0100

    update exports

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import type { BuxtRequest, BuxtResponse, Route, RoutePath } from "index.d";
 
 const factoryFunction = BuxtServer.createServer;
 
-export { factoryFunction as CreateServer };
-
 export type { BuxtResponse, BuxtRequest, Route, RoutePath };
-export default BuxtServer.createServer;
+
+export { factoryFunction as CreateServer };
+export default factoryFunction;
